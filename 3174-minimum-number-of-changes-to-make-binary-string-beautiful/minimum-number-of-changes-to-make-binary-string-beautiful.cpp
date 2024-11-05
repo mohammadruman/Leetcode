51d30bc9d287c224b717fab41f349bd7962cc465
Mohammad Ruman <88760648+mohammadruman@users.noreply.github.com>
2024-11-06 03:04:42 +0530

    Time: 2 ms (47.75%) | Memory: 12.2 MB (67.88%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int n = s.length();
-        int count =0;
-        int change=0;
-        char ch = s[0];
-        for(int i =0 ; i < n ; i++){
-            if(s[i]==ch){
-                count++;
-                continue;
-            }
-            //curr char is not equal so 
-            if(count%2==0){
-                count =1; //start new substring
-
-            }
-            else{
-                change++;
-                count =0;
-
-            }
-            ch = s[i]; //update the new char
+      int change =0;
+      for(int i =0 ; i <s.length();i+=2){
+        if(s[i]!=s[i+1]){
+            change++;
         }
+      }
         return change;
     }
 };
