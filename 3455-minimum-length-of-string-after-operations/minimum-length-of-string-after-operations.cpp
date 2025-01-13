31894a80db3665877f42e8a2f11ffd3189b8c9fb
Mohammad Ruman <88760648+mohammadruman@users.noreply.github.com>
2025-01-14 02:22:28 +0530

    Time: 27 ms (57.69%) | Memory: 32 MB (74.62%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int minimumLength(string s) {
         int ans=0;
+        int n = s.size();
        vector<int>v1(26,0);
        for(auto &i:s){
         v1[i-'a']++;
         if(v1[i-'a']>=3){
             v1[i-'a']-=2;
+            ans+=2;
         }
        }
-       for(int i =0; i < v1.size();i++){
-        ans+=v1[i];
-       }
-       
-       return ans;
+       return n-ans;
     }
 };
